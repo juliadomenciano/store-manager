0cbce718c42438d1d7204ad082e0addb9aaef4e1
Maju Domenciano <juliadomenciano@hotmail.com>
2022-06-29 14:39:27 -0300

    commit inicial

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
+app.get('/products', (_req, res) => {
+  res.send('oi');
+});
+
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
